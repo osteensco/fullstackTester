c073579194cff8b5376b3f463448e2501289970a
br0steen <osteensco@gmail.com>
2023-11-21 20:15:19 -0600

    golang server added

---
+++ golangTest/go.mod
@@ -0,0 +1,3 @@
+module osteensco/fullstackTester/golangTest
+
+go 1.21.0
---
+++ golangTest/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+    "log"
+)
+
+func main() {
+
+    Port := "8080"
+
+    // http.Handle("/", http.FileServer(http.Dir("../vanillajs")))
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "OK")
+    })
+
+    log.Println("Server listening on "+ Port)
+
+    err := http.ListenAndServe(":"+Port, nil)
+	if err != nil {
+		log.Println("Error starting server:", err)
+	}
+
+}
